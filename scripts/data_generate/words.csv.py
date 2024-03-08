90840178cb84a9ffdfc201ac646e01d2b757b513
archibate <1931127624@qq.com>
2024-03-08 12:26:30 +0800

    words

---
+++ scripts/data_generate/words.csv.py
@@ -0,0 +1,15 @@
+import pickle
+
+tab = {}
+
+# pinyin_to_simplified.pickle can be obtained from: https://github.com/System-T/DimSim/raw/master/dimsim/data/pinyin_to_simplified.pickle
+with open('build/pinyin_to_simplified.pickle', 'rb') as f:
+    for pinyin, word in pickle.load(f).items():
+        pinyin = pinyin.lower().split()
+        pinyin = tuple(p.upper() if len(p) == 1 else p[:-1] if p[-1] in '12345' else p for p in pinyin)
+        tab.setdefault(pinyin, [])
+        tab[pinyin] += word
+
+with open('src/words.csv', 'w') as f:
+    for pinyin, word in tab.items():
+        f.write('{}\t{}\n'.format(' '.join(pinyin), ' '.join(word)))
